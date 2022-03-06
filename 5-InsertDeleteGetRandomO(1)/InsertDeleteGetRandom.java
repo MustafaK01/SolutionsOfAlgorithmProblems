27c2de1cce6d09f461671665e4f02ac3f4bb9ebd
Mustafa Köroğlu <62333156+MustafaK01@users.noreply.github.com>
2022-03-06 22:16:10 +0300

    Solution Of 5

---
+++ 5-InsertDeleteGetRandomO(1)/InsertDeleteGetRandom.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
+
+public class InsertDeleteGetRandom {
+	
+    public List<Integer> set = new ArrayList<Integer>();
+    public InsertDeleteGetRandom() {
+    }
+    
+    public boolean insert(int val) {
+    	if(!set.contains(val)) {
+        	if(set.size()==0) {
+                set.add(set.size(),val);
+                return true;
+        		}
+        	else if(set.size()>=1) {
+                set.add(set.size()-1,val);
+                return true;
+        	}
+    	}
+    	return false;
+    }
+    	
+    
+    public boolean remove(int val) {
+    	if(set.contains(val)) {
+    		set.remove(new Integer(val));
+    	}
+    	return false;    
+    }
+    
+    public int getRandom() {
+        Random rand = new Random();
+        int randomElement = set.get(rand.nextInt(set.size()));
+        return randomElement;
+    }
+
+}
